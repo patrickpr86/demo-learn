df1a6ec299f450b5b30ab5c653433b8fc94f53c9
patrickpr86 <sh4dbr@gmail.com>
2021-07-02 11:28:52 -0300

    Course entity

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,3 +13,8 @@ INSERT INTO user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO user_role (user_id, role_id) VALUES (3, 3);
+
+
+
+
+INSERT INTO course (name, img_uri, img_gray_uri) VALUES ('Bootcamp GO', 'https://storage.ning.com/topology/rest/1.0/file/get/8170567668?profile=RESIZE_710x', 'https://www.programmersought.com/images/892/768c04585a653ddef26cb15758166bac.png');
