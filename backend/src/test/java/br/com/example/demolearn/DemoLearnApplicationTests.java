47e99c192d9c5491adda63d8c851f62f55978f44
patrickpr86 <sh4dbr@gmail.com>
2021-07-02 10:18:34 -0300

    backend project created

---
+++ backend/src/test/java/br/com/example/demolearn/DemoLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.demolearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
